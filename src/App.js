4b00b130b4a1b7a6aa4e13e6d50007ef1a3518e9
bedhiefoumaima <oumaimadhief@gmail.com>
2021-08-11 12:20:51 +0100

    mycheckpointreact

---
+++ src/App.js
@@ -0,0 +1,190 @@
+import './App.css';
+
+import{Navbar,NavDropdown, Nav, Form,FormControl, Button, Card, Carousel,ButtonGroup, DropdownButton,Dropdown }  from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+      <Navbar className="Navbar" bg="light" expand="lg">
+  <Navbar.Brand href="#">Restaurant Tunisien</Navbar.Brand>
+  <Navbar.Toggle aria-controls="navbarScroll" />
+  <Navbar.Collapse id="navbarScroll">
+    <Nav
+      className="mr-auto my-2 my-lg-0"
+      style={{ maxHeight: '100px' }}
+      navbarScroll
+    >
+      <Nav.Link href="#action1">Home</Nav.Link>
+      <Nav.Link href="#action2">Link</Nav.Link>
+      <NavDropdown title="Link" id="navbarScrollingDropdown">
+        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
+      </NavDropdown>
+      <Nav.Link href="#" disabled>
+        Link
+      </Nav.Link>
+    </Nav>
+    <Form className="d-flex">
+      <FormControl
+        type="search"
+        placeholder="Search"
+        className="mr-2"
+        aria-label="Search"
+      />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>
+<>
+  <Card classname="Card"   border="primary" style={{ width: '18rem' }}>
+    <Card.Header>Header</Card.Header>
+    <Card.Body>
+      <Card.Title>Primary Card Title</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </Card.Text>
+    </Card.Body>
+  </Card>
+  <br />
+
+  <Card border="secondary" style={{ width: '18rem' }}>
+    <Card.Header>Header</Card.Header>
+    <Card.Body>
+      <Card.Title>Secondary Card Title</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </Card.Text>
+    </Card.Body>
+  </Card>
+  <br />
+
+  <Card border="success" style={{ width: '18rem' }}>
+    <Card.Header>Header</Card.Header>
+    <Card.Body>
+      <Card.Title>Success Card Title</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </Card.Text>
+    </Card.Body>
+  </Card>
+  <br />
+
+  <Card border="danger" style={{ width: '18rem' }}>
+    <Card.Header>Header</Card.Header>
+    <Card.Body>
+      <Card.Title>Danger Card Title</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </Card.Text>
+    </Card.Body>
+  </Card>
+  <br />
+
+  <Card border="warning" style={{ width: '18rem' }}>
+    <Card.Header>Header</Card.Header>
+    <Card.Body>
+      <Card.Title>Warning Card Title</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </Card.Text>
+    </Card.Body>
+  </Card>
+  <br />
+
+  <Card border="info" style={{ width: '18rem' }}>
+    <Card.Header>Header</Card.Header>
+    <Card.Body>
+      <Card.Title>Info Card Title</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </Card.Text>
+    </Card.Body>
+  </Card>
+  <br />
+
+  <Card border="dark" style={{ width: '18rem' }}>
+    <Card.Header>Header</Card.Header>
+    <Card.Body>
+      <Card.Title>Dark Card Title</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </Card.Text>
+    </Card.Body>
+  </Card>
+  <br />
+
+  <Card border="light" style={{ width: '18rem' }}>
+    <Card.Header>Header</Card.Header>
+    <Card.Body>
+      <Card.Title>Light Card Title</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </Card.Text>
+    </Card.Body>
+  </Card>
+  <br />
+</>
+<Carousel className="car">
+  <Carousel.Item>
+    <img
+      className="d-block "
+      src="https://i.pinimg.com/originals/b1/74/eb/b174eb0c444bc7b028fe00fead7f89f3.jpg"
+      alt="First slide"
+    />
+    <Carousel.Caption className = "caption">
+      <h3>Plat Tunisien</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block"
+      src="https://www.onamangepourvous.tn/wp-content/uploads/2019/12/nouacer.jpg"
+      alt="Second slide"
+    />
+
+    <Carousel.Caption className = "caption">
+      <h3>Couscous</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block"
+      src="https://img-global.cpcdn.com/recipes/dcfceaeee08d2cbd/751x532cq70/ojja-plat-tunisien-photo-principale-de-la-recette.jpg"
+      alt="Third slide"
+      
+    />
+
+    <Carousel.Caption className = "caption">
+      <h3>Ojja</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+<ButtonGroup>
+  <Button>1</Button>
+  <Button>2</Button>
+
+  <DropdownButton as={ButtonGroup} title="Dropdown" id="bg-nested-dropdown">
+    <Dropdown.Item eventKey="1">Dropdown link</Dropdown.Item>
+    <Dropdown.Item eventKey="2">Dropdown link</Dropdown.Item>
+  </DropdownButton>
+</ButtonGroup>
+    </div>
+
+  );
+}
+
+export default App;
